4c9212a0e31a88e182a0f44ac7ff852cca49e308
Shukla <advaits@amazon.com>
2015-02-08 22:27:10 -0800

    Adding second activity chapter work

---
+++ app/src/main/java/geoquiz/android/advaits/com/myapplication/TrueFalse.java
@@ -0,0 +1,31 @@
+package geoquiz.android.advaits.com.myapplication;
+
+/**
+ * Created by advaits on 1/15/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setmTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
